b125cc834ab8985688d722ae2aa5d853ce0a2e33
jesse <u1110728@utah.edu>
2023-09-18 20:26:39 -0600

    Created Scenes folder for scenes. --created home scene which will contain the home map scene. --Started bottom tab navigator.

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+import HomeScene from './Scenes/HomeScene/HomeScene';
+
+const Tab = createBottomTabNavigator();
+
 
 export default function App() {
+
+  
+
   return (
-    <View style={styles.mainContainer}>
-      <Text style={styles.textInfo}>ghjhj</Text>
-      <Text style={styles.textInfo}>ghjhj</Text>
-      <Text style={styles.textInfo}>ghjhj</Text>
-      <Text style={styles.textInfo}>ghjg</Text>
-      {/* <MapView style={styles.mapContainer} provider={PROVIDER_GOOGLE}/> */}
-      <MapView
-      style={{width:400,height:200}}
-      provider={PROVIDER_GOOGLE}
-      ></MapView>
-    </View>
-  );
+  //   <View style={styles.mainContainer}>
+  //     <Text style={styles.textInfo}>ghjhj</Text>
+  //     <Text style={styles.textInfo}>ghjhj</Text>
+  //     <Text style={styles.textInfo}>ghjhj</Text>
+  //     <Text style={styles.textInfo}>ghjg</Text>
+  //     {/* <MapView style={styles.mapContainer} provider={PROVIDER_GOOGLE}/> */}
+  //     <MapView
+  //     style={{width:400,height:200}}
+  //     provider={PROVIDER_GOOGLE}
+  //     ></MapView>
+  //   </View>
+  <NavigationContainer>
+  <Tab.Navigator>
+  <Tab.Screen name="Home" component={HomeScene} />
+    </Tab.Navigator>
+  </NavigationContainer>
+   );
 }
 
 const styles = StyleSheet.create({
---
+++ Scenes/HomeScene/HomeScene.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+const HomeScene = () => {
+
+    return (
+        <View>
+        <Text>home</Text>
+        </View>
+    );
+}
+
+export default HomeScene;
