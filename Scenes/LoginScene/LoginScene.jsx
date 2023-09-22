dc40167278ec71c80ccb0d6a96decf118976bd43
jesse <u1110728@utah.edu>
2023-09-21 18:13:42 -0600

    --Client is now communicating using login protocol with Beacon server.

--- Scenes/LoginScene/LoginScene.jsx
+++ Scenes/LoginScene/LoginScene.jsx
@@ -1,47 +1,92 @@
 import { View, Button } from 'react-native';
+import { useState, useEffect } from 'react';
 
 import styles from "../../Styles";
 
 import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin';
-
-
-//Attempt to sign in with Google
-const signInGoogle = async () => {
-    console.log("in sign in google");
-    try {
-        await GoogleSignin.hasPlayServices();
-        console.log("after play serveicees");
-        const userInfo = await GoogleSignin.signIn();
-        console.log("ass");
-        //setState({ userInfo });
-        console.log(userInfo);
-      } catch (error) {
-        if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-          // user cancelled the login flow
-        } else if (error.code === statusCodes.IN_PROGRESS) {
-          // operation (e.g. sign in) is in progress already
-        } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-          // play services not available or outdated
-        } else {
-          // some other error happened
-          console.log();
-        }
-      }    
-}
-
-const signOut = async () => {
-    try {
-        await GoogleSignin.signOut();
-        console.log("signed out");
-
-        //setState({ user: null }); // Remember to remove the user from your app's state as well
-    } catch (error) {
-        console.error(error);
-    }
-};
+import { useRef } from 'react/cjs/react.production.min';
 
 const LoginScene = () => {
-    
+
+    const [user, setUser] = useState(null);
+
+    //Attempt to sign in with Google
+    const signInGoogle = async () => {
+        console.log("in sign in google");
+        try {
+            await GoogleSignin.hasPlayServices();
+            
+            const userInfo = await GoogleSignin.signIn();
+            //const userInfo = await getCurrentUser();
+            
+            console.log("after userinfo");
+            console.log(userInfo);
+            setUser( userInfo );
+           
+        } catch (error) {
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                // user cancelled the login flow
+            } else if (error.code === statusCodes.IN_PROGRESS) {
+                // operation (e.g. sign in) is in progress already
+            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                // play services not available or outdated
+            } else {
+                // some other error happened
+                console.log();
+            }
+        }
+    }
+
+    const signOut = async () => {
+        try {
+            await GoogleSignin.signOut();
+            console.log("signed out");
+
+            //setState({ user: null }); // Remember to remove the user from your app's state as well
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    //Get user details for Beacon and then navigate to home page
+    const getUser = async () => {
+        console.log("in get user");
+         console.log(user.idToken);
+         console.log(user.user.email);
+        try {
+            const response = await fetch("http://192.168.86.123:3001/auth/login", {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${user.idToken}`,
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    nickName: user.user.name,
+                    email: user.user.email,
+                })
+            })
+
+            const loggedIn = await response.json();
+            console.log("befer nick name");
+            console.log(loggedIn);
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+
+    useEffect(() => {
+      
+        if (user != null) {
+            console.log("mmmmm");
+       console.log(user.user.name);
+       console.log(user.user.email);
+            getUser(user);
+        }
+    }, [user]);
+
+
+
     GoogleSignin.configure({
         webClientId: "816978156146-1otrk1lmr0fhmp7de1oevblo6hinrhdr.apps.googleusercontent.com",
         offlineAccess: true,
@@ -52,11 +97,11 @@ const LoginScene = () => {
             <GoogleSigninButton
                 size={GoogleSigninButton.Size.Wide}
                 color={GoogleSigninButton.Color.Dark}
-               onPress={signInGoogle}
+                onPress={signInGoogle}
 
             />
 
-<Button title={"out"} onPress={signOut} >out</Button>
+            <Button title={"out"} onPress={signOut} >out</Button>
         </View>
     )
 };
