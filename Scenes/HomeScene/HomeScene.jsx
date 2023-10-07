80c89435bfb89ae3baf9c505829befd503cc3ea8
jesse <u1110728@utah.edu>
2023-10-06 22:00:27 -0600

    --Added ImagePicker and Location from Expo to allow for device location and uploading photos. --HomeScene now uses user location to update coordinates into state for use.

--- Scenes/HomeScene/HomeScene.jsx
+++ Scenes/HomeScene/HomeScene.jsx
@@ -1,22 +1,60 @@
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text } from "react-native";
 import { useSelector } from "react-redux";
 
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 import styles from '../../Styles.js';
 
 const HomeScene = () => {
 
     const nickName = useSelector((state) => state.user.nickName);
-    console.log(nickName);
+
+    const [location, setLocation] = useState({
+        latitude: 140.6717733,
+        longitude: -211.9493132,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+    });
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            };
+
+            let loc = await Location.getCurrentPositionAsync({});
+
+            setLocation({
+                ...location,
+                latitude: loc.coords.latitude,
+                longitude: loc.coords.longitude
+            });
+        })();
+    }, []);
+
+    let text = 'waiting...';
+    if (errorMsg) {
+        text = errorMsg;
+    } else if (location) {
+
+    }
+
 
     return (
         <View style={styles.mainContainer}>
             <Text style={styles.mainText}>{nickName}</Text>
-            <Text style={styles.mainText}>jkhjhk</Text>
-            <Text style={styles.mainText}>jkhjhk</Text>
-            <Text style={styles.mainText}>mmmmm</Text>
-            <MapView style={styles.mapContainer} provider={PROVIDER_GOOGLE} />
+            <Text style={styles.mainText}> {text} </Text>
+            <MapView
+                style={styles.mapContainer}
+                provider={PROVIDER_GOOGLE}
+
+                region={location}
+            />
 
         </View>
     );
